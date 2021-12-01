b4adcde01b2d1148bb1ec8b62487b1b7619c38b7
Andres Romero <andbrew@pm.me>
2021-12-01 12:27:41 -0700

    Add query(args) function

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
-var mysql = require('mysql');
+function query(args) {
+	var querys = {
+		cast:
+		`SELECT actor_id, name
+		FROM actor LEFT JOIN person
+		ON actor.actor_id = person.id
+		WHERE movie_id = '${args["movie_id"]}';`
+	};
+	return querys[args["alias"]];
+}
+
+var mysql = require("mysql");
 
 var balux = mysql.createConnection({
 	host: "localhost",
@@ -12,7 +23,10 @@ balux.connect(err => {
 	console.log("Connected!");
 });
 
-balux.query('SELECT COUNT(*) FROM movie;', (err, result) => {
+balux.query(query({
+	alias: "cast",
+	movie_id: "0109830"
+}), (err, result) => {
 	if(err) throw err;
 	console.log(result);
 });
