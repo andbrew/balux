6590326e5df3fd6fa081a637b96d1495dfb76029
ATony23 <83042213+ATony23@users.noreply.github.com>
2021-12-01 21:02:50 -0700

    Add files via upload

---
+++ Cliente - Andrés/src/views/Movies.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="services">
+    <MoviesComponent :moviesList="movies" />
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+import MoviesComponent from "@/components/MoviesComponent.vue";
+
+export default {
+  name: "Movies",
+  components: {
+    MoviesComponent,
+  },
+  data() {
+    return {
+      movies: null,
+    };
+  },
+  mounted() {
+    this.getMovies();
+  },
+  methods: {
+    getMovies() {
+      axios
+        .get("http://localhost:3001/movies/mostrarTodas")
+        .then((result) => {
+          this.movies = result.data.data;
+          console.log('movies: ', result.data.data);
+        })
+        .catch((e) => console.log(e));
+    },
+  },
+};
+</script>
