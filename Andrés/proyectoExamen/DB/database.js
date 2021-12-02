dd248b71d542c11e6a1ec7baed88e3ee7a0c5151
Andres Romero <andbrew@pm.me>
2021-12-02 11:06:12 -0700

    Configure to local credentials

--- Andrés/proyectoExamen/DB/database.js
+++ Andrés/proyectoExamen/DB/database.js
@@ -3,8 +3,8 @@ const boxen = require('boxen');
 
 const mysqlConnection = mysql.createConnection({
     host: 'localhost',
-    user: 'root',
-    password: 'Genetica12018',
+    user: 'alvin',
+    password: 'galletaBernardi',
     database: 'balux',
     multipleStatements: true
 });
